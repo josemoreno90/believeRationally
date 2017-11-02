7475aa7b7aa483aa9bb7b3a49f2c02f684d12a6d
Jose Moreno <morenojr90@yahoo.com>
2017-11-02 11:48:31 -0700

    Add header and footer data, navbar and routes for homepage. Link section page to "start learning" button.

--- server.js
+++ server.js
@@ -8,6 +8,20 @@ app.get('/', (req,res) => {
   res.render("index")
 })
 
+app.get('/about', (req,res) => {
+  res.render("about")
+})
+
+app.get('/sections', (req,res) => {
+  res.render("sections")
+})
+
+app.get('/sections/:lesson', (req,res) => {
+  const id = parseInt(req.params.id);
+
+  res.render("lesson")
+})
+
 
 app.listen(3000, () => {
   console.log('We Hear You!')
