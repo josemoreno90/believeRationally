06a40a1c57bb1a8ed9f3655a32bb143ecf685422
Jose Moreno <morenojr90@yahoo.com>
2017-11-03 20:40:06 -0700

    Update package.json and sections route

--- public/apiFunctions.js
+++ public/apiFunctions.js
@@ -1,5 +1,5 @@
-// const ypi = require('youtube-playlist-info');
 const Youtube = require('simple-youtube-api')
+//ApiKey...
 const youtube = new Youtube('AIzaSyD8Cu_3KvtA_1XWO0lQ4ee06Ikce1cUnds');
 
 module.exports = {
