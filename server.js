efef07c896ee3a3dabc1233b7f1e2d47efb05a2c
Jose Moreno <morenojr90@yahoo.com>
2018-04-23 23:19:34 -0700

    y

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const apiFunctions = require('./public/apiFunctions');
 var sm = require('sitemap')
 // Creates a sitemap object given the input configuration with URLs
 var sitemap = sm.createSitemap ({
-  const url = "https://wewillcode.com"
+  const url = "https://wewillcode.com";
   hostname: 'https://shrouded-bayou-12615.herokuapp.com/',
   cacheTime: 600000,        // 600 sec - cache purge period
   urls: [
