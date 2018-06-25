6fa71f880b4b299256720464f398d78941ee9d04
Jose Moreno <morenojr90@yahoo.com>
2018-06-25 10:08:20 -0700

    Fix some weird issue by erasing from lesson.ejs line 20is and 24is a +, and -, to a 'j'

--- server.js
+++ server.js
@@ -4,42 +4,10 @@ const apiFunctions = require('./public/apiFunctions');
 
 
 
-var sm = require('sitemap')
-// Creates a sitemap object given the input configuration with URLs
-var sitemap = sm.createSitemap ({
-  hostname: 'https://shrouded-bayou-12615.herokuapp.com/',
-  cacheTime: 600000,        // 600 sec - cache purge period
-  urls: [
-    { url: '/courses/',  changefreq: 'daily', priority: 0.3 },
-    { url: '/courses/CryptoZombiesTutorial(IntrotoSolidity)', priority: 0.3 },     // changefreq: 'weekly',  priority: 0.5
-    { url: '/courses/AprendeelHTMLyCSS2018enCodecademy',  changefreq: 'daily', priority: 0.3 },
-    { url: '/courses/LearnJavaOnCodecademy',  changefreq: 'daily', priority: 0.3 },
-    { url: '/courses/NodejsExpressTutorial',  changefreq: 'daily', priority: 0.3 },
-    { url: '/courses/freeCodeCampReviewhtml&css',  changefreq: 'daily', priority: 0.3 },
-    { url: '/courses/ResponsiveDesignwithBootstrapfreeCodeCampreview',  changefreq: 'daily', priority: 0.3 },
-    { url: '/courses/LearnResponsiveDesignCodecademyWebDevTutorials',  changefreq: 'daily', priority: 0.3 },
-    { url: '/courses/LearnCSSCodecademy',  changefreq: 'daily', priority: 0.3 },
-    { url: '/courses/LearnSQLCodecademy',  changefreq: 'daily', priority: 0.3 },
-    { url: '/courses/HowToUseAPIswithJavaScript',  changefreq: 'daily', priority: 0.3 },
-    { url: '/courses/LearnHTML&CSSIn2Hours&CSSFrameworkBootstrap',  changefreq: 'daily', priority: 0.3 },
-    { url: '/courses/ObjectOrientedandFunctionalProgramming',  changefreq: 'daily', priority: 0.3 },
-    { url: '/courses/JSONAPIsandAJAX',  changefreq: 'daily', priority: 0.3 }
-
-
-  ]
-});
-
 app.use(express.static('public'))
 app.set('view engine', 'ejs')
 
 app.get('/sitemap.xml', function(req, res) {
-  // sitemap.toXML( function (err, xml) {
-  //     if (err) {
-  //       return res.status(500).end();
-  //     }
-  //     res.header('Content-Type', 'application/xml');
-  //     res.send( xml );
-  // });
   res.render("sitemap")
 
 });
